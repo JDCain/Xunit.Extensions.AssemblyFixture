92be7fce05698ebd6bb059783f9b8303ed467edc
Cyrille NDOUMBE <candoumbe@users.noreply.github.com>
2021-12-07 16:33:50 +0100

    Added handling of fixtures that implements `IAsyncLifetime`, `IAsyncDisposable` (#6)
    Added parallelization of initialization and destruction of fixtures.

--- src/TestAssemblyRunner.cs
+++ src/TestAssemblyRunner.cs
@@ -3,40 +3,115 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Xunit.Abstractions;
 using Xunit.Sdk;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Xunit.Extensions.AssemblyFixture
 {
-	class TestAssemblyRunner : XunitTestAssemblyRunner
-	{
-		readonly Dictionary<Type, object> assemblyFixtureMappings = new Dictionary<Type, object>();
+    class TestAssemblyRunner : XunitTestAssemblyRunner
+    {
+#if NET5_0_OR_GREATER
+        private readonly Dictionary<Type, object> _assemblyFixtureMappings = new();
+#else
+        private readonly Dictionary<Type, object> _assemblyFixtureMappings = new Dictionary<Type, object>();
 
-		public TestAssemblyRunner(ITestAssembly testAssembly,
-			IEnumerable<IXunitTestCase> testCases,
-			IMessageSink diagnosticMessageSink,
-			IMessageSink executionMessageSink,
-			ITestFrameworkExecutionOptions executionOptions)
-			: base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions)
-		{
-		}
+#endif
 
-		protected override Task BeforeTestAssemblyFinishedAsync()
-		{
-			// Make sure we clean up everybody who is disposable, and use Aggregator.Run to isolate Dispose failures
-			foreach (var disposable in assemblyFixtureMappings.Values.OfType<IDisposable>())
-				Aggregator.Run(disposable.Dispose);
+        public TestAssemblyRunner(
+            ITestAssembly testAssembly,
+            IEnumerable<IXunitTestCase> testCases,
+            IMessageSink diagnosticMessageSink,
+            IMessageSink executionMessageSink,
+            ITestFrameworkExecutionOptions executionOptions)
+            : base(testAssembly, testCases, diagnosticMessageSink, executionMessageSink, executionOptions)
+        { }
 
-			return base.BeforeTestAssemblyFinishedAsync();
-		}
+        ///<inheritdoc/>
+        protected override async Task AfterTestAssemblyStartingAsync()
+        {
+            // Let everything initialize
+            await base.AfterTestAssemblyStartingAsync().ConfigureAwait(false);
+
+            // Go find all the AssemblyFixtureAttributes adorned on the test assembly
+            await Aggregator.RunAsync(async () =>
+            {
+                ISet<Type> assemblyFixtures = new HashSet<Type>(((IReflectionAssemblyInfo)TestAssembly.Assembly).Assembly
+                    .GetTypes()
+                    .Select(type => type.GetInterfaces())
+                    .SelectMany(x => x)
+                    .Where(@interface => @interface.IsAssignableToGenericType(typeof(IAssemblyFixture<>)))
+                    .ToArray());
+
+                // Instantiate all the fixtures
+                foreach (Type fixtureAttribute in assemblyFixtures)
+                {
+                    Type fixtureType = fixtureAttribute.GetGenericArguments()[0];
+                    var hasConstructorWithMessageSink = fixtureType.GetConstructor(new[] { typeof(IMessageSink) }) != null;
+                    _assemblyFixtureMappings[fixtureType] = hasConstructorWithMessageSink
+                        ? Activator.CreateInstance(fixtureType, ExecutionMessageSink)
+                        : Activator.CreateInstance(fixtureType);
+                }
+
+                // Initialize IAsyncLifetime fixtures
+                foreach (IAsyncLifetime asyncLifetime in _assemblyFixtureMappings.Values.OfType<IAsyncLifetime>())
+                {
+                    await Aggregator.RunAsync(async () => await asyncLifetime.InitializeAsync().ConfigureAwait(false)).ConfigureAwait(false);
+                }
+            }).ConfigureAwait(false);
+        }
+
+        protected override async Task BeforeTestAssemblyFinishedAsync()
+        {
+            // Make sure we clean up everybody who is disposable, and use Aggregator.Run to isolate Dispose failures
+            Parallel.ForEach(_assemblyFixtureMappings.Values.OfType<IDisposable>(),
+                             disposable => Aggregator.Run(disposable.Dispose));
 
 
-		protected override Task<RunSummary> RunTestCollectionAsync(IMessageBus messageBus,
-																   ITestCollection testCollection,
-																   IEnumerable<IXunitTestCase> testCases,
-																   CancellationTokenSource cancellationTokenSource)
-		{
-			return new TestCollectionRunner(assemblyFixtureMappings, testCollection, testCases, DiagnosticMessageSink, messageBus, TestCaseOrderer, new ExceptionAggregator(Aggregator), cancellationTokenSource).RunAsync();
-		}
-	}
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+
+#if NET6_0_OR_GREATER
+               await Parallel.ForEachAsync(_assemblyFixtureMappings.Values.OfType<IAsyncDisposable>(),
+                                  async (disposable, _) => await Aggregator.RunAsync(async () => await disposable.DisposeAsync().ConfigureAwait(false))
+                                                                           .ConfigureAwait(false));
+#else
+            Parallel.ForEach(_assemblyFixtureMappings.Values.OfType<IAsyncDisposable>(),
+                             async (disposable, _) => await Aggregator.RunAsync(async () => await disposable.DisposeAsync().ConfigureAwait(false)));
+    
+#endif
+
+#endif
+
+
+#if NET6_0_OR_GREATER
+               await Parallel.ForEachAsync(_assemblyFixtureMappings.Values.OfType<IAsyncLifetime>(),
+                                  async (disposable, _) => await Aggregator.RunAsync(async () => await disposable.DisposeAsync().ConfigureAwait(false))
+                                                                           .ConfigureAwait(false));
+#else
+            Parallel.ForEach(_assemblyFixtureMappings.Values.OfType<IAsyncLifetime>(),
+                             async (disposable, _) => await Aggregator.RunAsync(async () => await disposable.DisposeAsync().ConfigureAwait(false)));
+    
+#endif
+
+            await base.BeforeTestAssemblyFinishedAsync().ConfigureAwait(false);
+        }
+
+        protected override async Task<RunSummary> RunTestCollectionAsync(
+            IMessageBus messageBus,
+            ITestCollection testCollection,
+            IEnumerable<IXunitTestCase> testCases,
+            CancellationTokenSource cancellationTokenSource)
+            => await new TestCollectionRunner(
+                _assemblyFixtureMappings,
+                testCollection,
+                testCases,
+                DiagnosticMessageSink,
+                messageBus,
+                TestCaseOrderer,
+                new ExceptionAggregator(Aggregator),
+                cancellationTokenSource)
+            .RunAsync().ConfigureAwait(false);
+    }
 }
---
+++ src/TypeExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace System
+{
+    /// <summary>
+    /// Extension methods for <see cref="Type"/>.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Checks if an instance of <paramref name="givenType"/> can be assigned to a type <paramref name="genericType"/>.
+        /// </summary>
+        /// <param name="givenType">The type under test</param>
+        /// <param name="genericType">The targeted type</param>
+        /// <returns><c>true</c> if <paramref name="genericType"/> is an ancestor of <paramref name="givenType"/> and <c>false</c> otherwise.</returns>
+        public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
+            => givenType is not null && genericType is not null
+               && (givenType == genericType || givenType.MapsToGenericTypeDefinition(genericType)
+                   || givenType.HasInterfaceThatMapsToGenericTypeDefinition(genericType)
+                   || givenType.GetTypeInfo().BaseType.IsAssignableToGenericType(genericType));
+
+        private static bool HasInterfaceThatMapsToGenericTypeDefinition(this Type givenType, Type genericType)
+        => givenType is not null && genericType is not null && givenType
+                .GetTypeInfo()
+#if NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_3
+
+                .ImplementedInterfaces
+#else
+            .GetInterfaces()
+#endif
+            .Where(it => it.GetTypeInfo().IsGenericType)
+                .Any(it => it.GetGenericTypeDefinition() == genericType);
+
+        private static bool MapsToGenericTypeDefinition(this Type givenType, Type genericType)
+            => givenType is not null
+               && genericType?.GetTypeInfo().IsGenericTypeDefinition == true
+               && givenType.GetTypeInfo().IsGenericType
+               && givenType.GetGenericTypeDefinition() == genericType;
+
+        /// <summary>
+        /// Tests if <paramref name="type"/> is an anonymous type
+        /// </summary>
+        /// <param name="type">The type under test</param>
+        /// <returns><c>true</c>if <paramref name="type"/> is an anonymous type and <c>false</c> otherwise</returns>
+        public static bool IsAnonymousType(this Type type)
+        {
+            bool hasCompilerGeneratedAttribute = type?.GetTypeInfo()?.GetCustomAttributes(typeof(CompilerGeneratedAttribute), false)?.Any() ?? false;
+            bool nameContainsAnonymousType = type?.FullName.Contains("AnonymousType") ?? false;
+
+            return hasCompilerGeneratedAttribute && nameContainsAnonymousType;
+        }
+    }
+}
